9cb316e2397950d2f85f0113ce6dd1fe17111db0
SergiiS95 <SSirota1995@gmail.com>
2023-05-24 12:36:08 +0300

    hm11

---
+++ src/index.js
@@ -0,0 +1,129 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import GalleryImage from './js/GalleryImage.js';
+import LoadMoreBtn from './js/LoadMoreBtn.js';
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+
+const newGallery = new GalleryImage();
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', isHidden: true });
+const data = {};
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.button.addEventListener('click', fetchData);
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+  const value = form.elements.searchQuery.value.trim();
+
+  if (!value) Notiflix.Report.warning('No value!');
+  else {
+    newGallery.searchQuery = value;
+    newGallery.resetPage();
+    gallery.innerHTML = '';
+    fetchData().finally(() => form.reset());
+    loadMoreBtn.show();
+  }
+}
+
+async function fetchData() {
+  loadMoreBtn.disable();
+  try {
+    const data = await newGallery.getImage();
+    const totalHits = Number(data.totalHits);
+    const page = newGallery.page - 1;
+    const hits = data.hits;
+
+    if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMoreBtn.hide();
+      return;
+    }
+    if (page === 1) {
+      Notiflix.Notify.success(`"Hooray! We found ${totalHits} images."`);
+    }
+
+    if (hits.length < 40) {
+      Notiflix.Notify.failure(
+        "   'We're sorry, but you've reached the end of search results.'"
+      );
+      loadMoreBtn.hide();
+    }
+
+    gallery.insertAdjacentHTML('beforeend', createMarcups(hits));
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
+      animationSpeed: 250,
+    });
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (err) {
+    onError(err);
+  }
+  loadMoreBtn.enable();
+}
+
+function createMarcups(hits) {
+  return hits.reduce((markup, hit) => markup + createMarcup(hit), '');
+}
+
+function createMarcup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${largeImageURL}">
+    
+    <img src=${webformatURL} alt=${tags} loading="lazy" />
+  
+     </a>
+     <div class="info">
+      <p class="info-item">
+        <b>Likes</b><br> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b><br> ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b><br> ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b> <br>${downloads}
+      </p>
+    </div>
+
+  
+</li>`;
+}
+
+function onError(err) {
+  console.error(err);
+  loadMoreBtn.hide();
+}
+
+function handleScroll() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    fetchData();
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
---
+++ src/js/GalleryImage.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '36598213-1c05ffc9aa6b3be50b197b0cb';
+
+export default class GalleryImage{
+    constructor() {
+        this.page = 1;
+        this.searchQuery = "";
+    }
+
+    async getImage () {
+    const { data } = await axios.get(`${URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`);
+    this.incrementPage();
+        return data;
+}
+
+resetPage(){
+    this.page = 1;
+}
+
+incrementPage(){
+    this.page += 1;
+}
+
+
+}
+
